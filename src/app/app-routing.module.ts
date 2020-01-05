9bc65d8f3db51b00d43d5e81c7b174f3214f14df
fbicewind <939228292@qq.com>
2020-01-05 17:40:13 +0800

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './component/page/main/main.component';
+import { BlogComponent } from './component/page/blog/blog.component';
+import { AlbumComponent } from './component/page/album/album.component';
+import { MineComponent } from './component/page/mine/mine.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [{ path: '', redirectTo: '/main', pathMatch: 'full' },
+{ path: 'main', component: MainComponent },
+{ path: 'blog', component: BlogComponent },
+{ path: 'album', component: AlbumComponent },
+{ path: 'aboutMe', component: MineComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
